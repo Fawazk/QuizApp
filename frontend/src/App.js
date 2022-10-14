f8718dbcc078dfcff6a1ed469d4b905787db8b81
fawaz <fawazfkp2@gmail.com>
2022-10-14 23:57:57 +0530

    frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// import React, { useEffect, useState } from "react";
+// import "./styles.css";
+
+// export default function App() {
+//   const [counter, setCounter] = useState(90);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCounter((counter) => counter - 1);
+//     }, 1000);
+
+//     return () => {
+//       clearInterval(interval);
+//     };
+//   }, []);
+
+//   return <div className="App">{counter}</div>;
+// }
+
